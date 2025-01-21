d8067459a57e410a3300c7770480bf5505c05303
AlvaroRGZ <alvarorgten@gmail.com>
2025-01-21 10:34:38 +0000

    Improved top and side panels

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import './Home.css';
+import React, { FC } from "react";
+import "./Home.css";
 
 import { CiGlobe } from "react-icons/ci";
 import { FaGithub, FaHeart } from "react-icons/fa";
@@ -10,37 +10,48 @@ const Home: FC<HomeProps> = () => (
   <div className="Home">
     <img
       className="about-logo"
-      src="src/assets/teidesat_logo.png"  // Asegúrate de que la ruta sea correcta
+      src="src/assets/teidesat_logo.png"
       alt="TeideSat Logo"
     />
     <div className="central-text">
       <p>
-        Space is an environment that generates immense fascination for human beings because of the many secrets that it still holds. With the evolution of new technologies, possibilities increase and new horizons are opened. From this point of view, our association is born: Hyperspace Canarias, created with the aim to develop several space related projects.
+        Space is an environment that generates immense fascination for human
+        beings because of the many secrets that it still holds. With the
+        evolution of new technologies, possibilities increase and new horizons
+        are opened. From this point of view, our association is born: Hyperspace
+        Canarias, created with the aim to develop several space related
+        projects.
       </p>
       <p>
-        We are a group of students from the Universidad de La Laguna studying in different faculties and branches like astrophysics, physics, mathematics and several other engineering subjects.
+        We are a group of students from the Universidad de La Laguna studying in
+        different faculties and branches like astrophysics, physics, mathematics
+        and several other engineering subjects.
       </p>
       <p>
-        The European Space Agency (ESA) pose a challenge to all the European Universities’ students and teachers so they can learn how to design and build a nano-satellite. Ours will be oriented to test optical communications without the need of lasers between Earth and space.
+        The European Space Agency (ESA) pose a challenge to all the European
+        Universities’ students and teachers so they can learn how to design and
+        build a nano-satellite. Ours will be oriented to test optical
+        communications without the need of lasers between Earth and space.
       </p>
       <p>
-        With that in mind <strong>Fomalhaut</strong> is developed as our main desktop application.
+        With that in mind <strong>Fomalhaut</strong> is developed as our main
+        desktop application.
       </p>
-    </div>  
+    </div>
     <div className="about-links">
       <div className="button-link">
         <a href="https://github.com/Teidesat/Fomalhaut">
-          <FaGithub/>  {/* Usando correctamente el icono de GitHub */}
+          <FaGithub />
         </a>
       </div>
       <div className="button-link">
         <a href="https://hyperspacegroup.com/">
-        <CiGlobe/> {/* Usando correctamente el icono del mundo */}
+          <CiGlobe />
         </a>
       </div>
       <div className="button-link">
         <a href="https://donorbox.org/hyperspace-canarias">
-        <FaHeart/> {/* Usando correctamente el icono del corazón */}
+          <FaHeart />
         </a>
       </div>
     </div>
--- src/components/TopPanel/TopPanel.tsx
+++ src/components/TopPanel/TopPanel.tsx
@@ -1,64 +1,57 @@
-import React, { FC, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './TopPanel.css';
+import React, { FC, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./TopPanel.css";
+
+import { FaSatelliteDish } from "react-icons/fa";
+import { MdSatelliteAlt } from "react-icons/md";
 
 interface TopPanelProps {}
 
 const TopPanel: FC<TopPanelProps> = () => {
-  // Función para formatear la hora actual
   const formatCurrentTime = (): string => {
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0'); // Añade ceros a la izquierda si es necesario
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    const day = now.getDate().toString().padStart(2, '0');
-    const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Los meses en JavaScript son de 0 a 11, así que sumamos 1
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+    const day = now.getDate().toString().padStart(2, "0");
+    const month = (now.getMonth() + 1).toString().padStart(2, "0");
     const year = now.getFullYear();
-    
-    // Obtén la zona horaria
+
     const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
     return `${day}/${month}/${year}, ${hours}:${minutes}:${seconds} ${timezone}`;
   };
 
-  // Estado para manejar la hora actual
   const [currentTime, setCurrentTime] = useState<string>(formatCurrentTime());
 
-  // Efecto para actualizar la hora cada segundo
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentTime(formatCurrentTime());
-    }, 1000); // Actualiza cada segundo
+    }, 1000);
 
-    return () => clearInterval(interval); // Limpia el intervalo al desmontar el componente
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="TopPanel">
-      {/* Logo */}
-      <div className="col-1">
-        <Link to="/">
-          <img
-            className="corner-icon"
-            src="src\assets\fomalhaut_logo.png"
-            alt="Fomalhaut Logo"
-          />
-        </Link>
-      </div>
-
-      {/* Connection Status */}
-      <div className="col-6 connection-status">
-        <span>
-          <FontAwesomeIcon icon="satellite-dish" />
-        </span>
-        <span>Ground Station</span>
-        <span className="status-text-good">Online</span>
-        <span>
-          <FontAwesomeIcon icon="satellite" />
-        </span>
-        <span>Next Satellite Window</span>
-        <span className="status-text-info">{currentTime}</span>
+      <Link to="/">
+        <img
+          className="corner-icon"
+          src="src\assets\fomalhaut_logo.png"
+          alt="Fomalhaut Logo"
+        />
+      </Link>
+      <div className="system-status">
+        <div className="ground-station">
+          <FaSatelliteDish />
+          <p>Ground Station: </p>
+          <p className="ground-station-status">Online</p>
+        </div>
+        <div className="satellite">
+          <MdSatelliteAlt />
+          <p>Next Satellite Window: </p>
+          <p className="satellite-status">{currentTime}</p>
+        </div>
       </div>
     </div>
   );
